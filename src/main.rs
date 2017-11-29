33b5d5c5a9912f6a486e631db9f66e803e561150
Ben Boxler <benjaminboxler@gmail.com>
2017-11-29 17:34:57 +0000

    Finished gravitational potential

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     setup_menu();
     loop {
-        println!("Please input either 1 or 2 now.");
+        println!("Please input either 1, 2 or 3 now.");
         let mut choice = String::new();
         io::stdin().read_line(&mut choice)
             .expect("Please make a choice.");
@@ -26,6 +26,10 @@ fn main() {
                 calculate_potential_energy();
                 break
             },
+            3 => { 
+                calculate_work_done();
+                break
+            }
             _ => {
                 println!("This wasn't a valid choice");
                 continue
@@ -41,17 +45,96 @@ fn setup_menu() {
     println!("What type of energy would you like to calculate?");
     println!("  1) Kinetic");
     println!("  2) Potential");
+    println!("  3) Work done");
 }
+
+ fn calculate_kinetic_energy() {
+    println!();
+    println!();
+    println!("You are calculating kinetic energy.");
+    let velocity = get_value(String::from("velocity in m/s^2"));
+    let mass = get_value(String::from("mass in kg"));
+
+    let kinetic_energy:f64 = 0.5 * velocity * mass * mass;
+    println!("=======================");
+    println!("Kinetic Energy: {}j", kinetic_energy);
+    println!("=======================");
+ }
 
 fn calculate_potential_energy() {
     println!();
-    println!("You chose potential energy");
     println!();
+    println!("You chose potential energy");
+    println!("Are you calculating potential energy due to a spring or gravity?");
+    println!("  1) Spring");
+    println!("  2) Gravity");
+    loop {
+        println!("Please enter 1 or 2 now");
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice)
+            .expect("Not a number. Please type 1 or 2");
+        let choice = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number. Please typw 1 or 2");
+                continue
+            }
+        };
+        match choice {
+            1 => {
+                calculate_spring_energy();
+                break
+            },
+            2 => {
+                calculate_gravitational_energy();
+            },
+            _ => {
+                println!("Not a valid choice.");
+                continue
+            }
+        }
+    }
 }
 
-fn calculate_kinetic_energy() {
+fn get_yes_no_value() -> bool {
+    loop {
+        let mut is_on_earth = String::new();
+        io::stdin().read_line(&mut is_on_earth)
+            .expect("Please enter Y or n");
+        match is_on_earth.as_ref() {
+            "Y" => return true,
+            "n" => return false,
+            _ => {
+                println!("Please enter Y or n");
+                continue
+            }
+        };
+    };
+}
+
+fn calculate_gravitational_energy() {
+    println!("You are calculating gravitational potential energy");
+    println!("Are you on earth? (Y/n");
+    let is_on_earth:bool = get_yes_no_value();
+    let gravity:f64 = if is_on_earth {
+        9.81
+    } else {
+        get_value(String::from("strength of gravity in N"))
+    };
+    let height = get_value(String::from("height of the object, in meters"));
+    let mass = get_value(String::from("mass of the object in kg"));
+
+    let gravitational_potential_energy: f64 = mass * gravity * height;
+    println!("Gravitational Potential Energy: {}j", gravitational_potential_energy);
+}
+
+fn calculate_spring_energy() {
+    println!("You are calculating spring energy");
+}
+
+fn calculate_work_done() {
     println!();
-    println!("Now calculating kinetic energy.");
+    println!("Now calculating work done.");
     println!();
     let force:f64 = get_value(String::from("force in Newtons"));
     let distance:f64 = get_value(String::from("distance in metres"));
